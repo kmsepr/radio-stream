c9285c3d96df7510621fcccd957bcd6c446ee126
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-24 19:13:43 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -65,7 +65,7 @@ def generate_stream(url):
             [
                 "ffmpeg", "-reconnect", "1", "-reconnect_streamed", "1",
                 "-reconnect_delay_max", "10", "-fflags", "nobuffer", "-flags", "low_delay",
-                "-i", url, "-vn", "-ac", "1", "-b:a", "16k", "-buffer_size", "1024k", "-f", "mp3", "-"
+                "-i", url, "-vn", "-ac", "1", "-b:a", "40k", "-buffer_size", "1024k", "-f", "mp3", "-"
             ],
             stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=8192
         )
