d1b8fe9e7ab4442d722d4c658018981c88af3e42
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-27 17:52:13 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -6,56 +6,71 @@ app = Flask(__name__)
 
 # 📡 List of radio stations
 RADIO_STATIONS = {
-    "rubat_ataq": "http://stream.zeno.fm/5tpfc8d7xqruv",
-    "shahul_radio": "https://stream-150.zeno.fm/cynbm5ngx38uv?zs=Ktca5StNRWm-sdIR7GloVg",
-    "eram_fm": "http://icecast2.edisimo.com:8000/eramfm.mp3",
-    "abc_islam": "http://s10.voscast.com:9276/stream",
-    "al_sumood_fm": "http://us3.internet-radio.com/proxy/alsumoodfm2020?mp=/stream",
-    "nur_ala_nur": "http://104.7.66.64:8011/",
-    "ruqya_radio": "http://104.7.66.64:8004",
-    "manjeri_fm": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio101/chunklist.m3u8",
-    "seiyun_radio": "http://s2.radio.co/s26c62011e/listen",
-    "motivational_series": "http://104.7.66.64:8010",
-    "deenagers_radio": "http://104.7.66.64:8003/",
-    "real_fm": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio083/playlist.m3u8",
-    "noor_al_eman": "http://edge.mixlr.com/channel/boaht",
-    "radio_keralam": "http://ice31.securenetsystems.net/RADIOKERAL",
-    "al_nour": "http://audiostreaming.itworkscdn.com:9066/",
+
     "muthnabi_radio": "http://cast4.my-control-panel.com/proxy/muthnabi/stream",
-    "fm_gold": "https://airhlspush.pc.cdn.bitgravity.com/httppush/hispbaudio005/hispbaudio00564kbps.m3u8",
+    "radio_keralam": "http://ice31.securenetsystems.net/RADIOKERAL",
+    "malayalam_1": "http://167.114.131.90:5412/stream",
+    "radio_digital_malayali": "https://radio.digitalmalayali.in/listen/stream/radio.mp3",
     "malayalam_90s": "https://stream-159.zeno.fm/gm3g9amzm0hvv?zs-x-7jq8ksTOav9ZhlYHi9xw",
     "aural_oldies": "https://stream-162.zeno.fm/tksfwb1mgzzuv?zs=SxeQj1-7R0alsZSWJie5eQ",
     "radio_malayalam": "https://radiomalayalamfm.com/radio/8000/radio.mp3",
     "swaranjali": "https://stream-161.zeno.fm/x7mve2vt01zuv?zs-D4nK05-7SSK2FZAsvumh2w",
-    "sirat_al_mustaqim": "http://104.7.66.64:8091/stream",
-    "air_calicut": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio082/chunklist.m3u8",
+    "radio_beat_malayalam": "http://live.exertion.in:8050/radio.mp3",
+    "shahul_radio": "https://stream-150.zeno.fm/cynbm5ngx38uv?zs=Ktca5StNRWm-sdIR7GloVg",
+    "nonstop_hindi": "http://s5.voscast.com:8216/stream",
+    "fm_gold": "https://airhlspush.pc.cdn.bitgravity.com/httppush/hispbaudio005/hispbaudio00564kbps.m3u8",
+    
+    "motivational_series": "http://104.7.66.64:8010",
+    "deenagers_radio": "http://104.7.66.64:8003/",
+    "hajj_channel": "http://104.7.66.64:8005",
+    "abc_islam": "http://s10.voscast.com:9276/stream",
+   
+    "eram_fm": "http://icecast2.edisimo.com:8000/eramfm.mp3",
+    "al_sumood_fm": "http://us3.internet-radio.com/proxy/alsumoodfm2020?mp=/stream",
+    "nur_ala_nur": "http://104.7.66.64:8011/",
+    "ruqya_radio": "http://104.7.66.64:8004",
+    "seiyun_radio": "http://s2.radio.co/s26c62011e/listen",
+    "noor_al_eman": "http://edge.mixlr.com/channel/boaht",
     "sam_yemen": "https://edge.mixlr.com/channel/kijwr",
     "afaq": "https://edge.mixlr.com/channel/rumps",
     "alfasi_radio": "https://qurango.net/radio/mishary_alafasi",
     "tafsir_quran": "https://radio.quranradiotafsir.com/9992/stream",
-    "malayalam_1": "http://167.114.131.90:5412/stream",
-    "urdu_islamic_lecture": "http://144.91.121.54:27001/channel_02.aac",
-    "hob_nabi": "http://216.245.210.78:8098/stream",
-    "sanaa_radio": "http://dc5.serverse.com/proxy/pbmhbvxs/stream",
-    "radio_beat_malayalam": "http://live.exertion.in:8050/radio.mp3",
-    "nonstop_hindi": "http://s5.voscast.com:8216/stream",
-    "radio_digital_malayali": "https://radio.digitalmalayali.in/listen/stream/radio.mp3",
+    "sirat_al_mustaqim": "http://104.7.66.64:8091/stream",
     "river_nile_radio": "http://104.7.66.64:8087",
     "quran_radio_cairo": "http://n02.radiojar.com/8s5u5tpdtwzuv?listening-from-radio-garden-1620219571863&rj-ttl=5&rj-tok=AAABivzcwaEAh735PZBqcATySw",
     "quran_radio_nablus": "http://www.quran-radio.org:8002/",
-    "hajj_channel": "http://104.7.66.64:8005",
+    "al_nour": "http://audiostreaming.itworkscdn.com:9066/",
     "allahu_akbar_radio": "http://66.45.232.132:9996/stream",
     "omar_abdul_kafi_radio": "http://104.7.66.64:8007",
+    "urdu_islamic_lecture": "http://144.91.121.54:27001/channel_02.aac",
+    "hob_nabi": "http://216.245.210.78:8098/stream",
+    "sanaa_radio": "http://dc5.serverse.com/proxy/pbmhbvxs/stream",
+    "rubat_ataq": "http://stream.zeno.fm/5tpfc8d7xqruv",
+    "al_jazeera": "http://live-hls-audio-web-aja.getaj.net/VOICE-AJA/index.m3u8",
+    
+    
+    
+   
+    
+    
+    
+   
     "asianet_news": "https://vidcdn.vidgyor.com/asianet-origin/audioonly/chunks.m3u8",
-    "media_one": "http://ktv.im:8080/44444/44444/81777",
+    
+    "air_kavarati": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio189/chunklist.m3u8",
+    "air_calicut": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio082/chunklist.m3u8",
+    "manjeri_fm": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio101/chunklist.m3u8",
+    "real_fm": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio083/playlist.m3u8",
+
+    
     "safari_tv": "https://j78dp346yq5r-hls-live.5centscdn.com/safari/live.stream/chunks.m3u8",
     "victers_tv": "https://932y4x26ljv8-hls-live.5centscdn.com/victers/tv.stream/victers/tv1/chunks.m3u8",
-    "air_kavarati": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio189/chunklist.m3u8",
-    "al_jazeera": "http://live-hls-audio-web-aja.getaj.net/VOICE-AJA/index.m3u8",
-    "surya_comedy": "http://ktv.im:8080/44444/44444/81825",
-    "star_sports": "http://ktv.im:8080/44444/44444/5427",
-    "asianet_movies": "http://ktv.im:8080/44444/44444/81804",
-    "priyan_ottathilan": "http://filex.tv:8080/movie/asdf18/1234/262694.mkv"
+    
+    
+    
+   
+    "asianet_movies": "http://ktv.im:8080/44444/44444/81804"
+   
     
 
 }
