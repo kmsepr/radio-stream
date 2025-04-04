ffbdfc3b436b4674396de6b74fa76391112e633a
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-04-04 11:05:46 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,13 +1,11 @@
 import subprocess
 import time
-from flask import Flask, Response,
-send_file
+from flask import Flask, Response, send_file
 
 app = Flask(__name__)
 
 # 📡 List of radio stations
 RADIO_STATIONS = {
-
     "muthnabi_radio": "http://cast4.my-control-panel.com/proxy/muthnabi/stream",
     "radio_keralam": "http://ice31.securenetsystems.net/RADIOKERAL",
     "malayalam_1": "http://167.114.131.90:5412/stream",
@@ -20,12 +18,10 @@ RADIO_STATIONS = {
     "shahul_radio": "https://stream-150.zeno.fm/cynbm5ngx38uv?zs=Ktca5StNRWm-sdIR7GloVg",
     "nonstop_hindi": "http://s5.voscast.com:8216/stream",
     "fm_gold": "https://airhlspush.pc.cdn.bitgravity.com/httppush/hispbaudio005/hispbaudio00564kbps.m3u8",
-    
     "motivational_series": "http://104.7.66.64:8010",
     "deenagers_radio": "http://104.7.66.64:8003/",
     "hajj_channel": "http://104.7.66.64:8005",
     "abc_islam": "http://s10.voscast.com:9276/stream",
-   
     "eram_fm": "http://icecast2.edisimo.com:8000/eramfm.mp3",
     "al_sumood_fm": "http://us3.internet-radio.com/proxy/alsumoodfm2020?mp=/stream",
     "nur_ala_nur": "http://104.7.66.64:8011/",
@@ -38,7 +34,7 @@ RADIO_STATIONS = {
     "tafsir_quran": "https://radio.quranradiotafsir.com/9992/stream",
     "sirat_al_mustaqim": "http://104.7.66.64:8091/stream",
     "river_nile_radio": "http://104.7.66.64:8087",
-    "quran_radio_cairo": "http://n02.radiojar.com/8s5u5tpdtwzuv?listening-from-radio-garden-1620219571863&rj-ttl=5&rj-tok=AAABivzcwaEAh735PZBqcATySw",
+    "quran_radio_cairo": "http://n02.radiojar.com/8s5u5tpdtwzuv",
     "quran_radio_nablus": "http://www.quran-radio.org:8002/",
     "al_nour": "http://audiostreaming.itworkscdn.com:9066/",
     "allahu_akbar_radio": "http://66.45.232.132:9996/stream",
@@ -48,71 +44,42 @@ RADIO_STATIONS = {
     "sanaa_radio": "http://dc5.serverse.com/proxy/pbmhbvxs/stream",
     "rubat_ataq": "http://stream.zeno.fm/5tpfc8d7xqruv",
     "al_jazeera": "http://live-hls-audio-web-aja.getaj.net/VOICE-AJA/index.m3u8",
-   
-   
     "asianet_news": "https://vidcdn.vidgyor.com/asianet-origin/audioonly/chunks.m3u8",
-    
     "air_kavarati": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio189/chunklist.m3u8",
     "air_calicut": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio082/chunklist.m3u8",
     "manjeri_fm": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio101/chunklist.m3u8",
     "real_fm": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio083/playlist.m3u8",
-
-"vom_news": "https://psmnews.mv/stream/radio-dhivehi-raajjeyge-adu",
-
-
-    
+    "vom_news": "https://psmnews.mv/stream/radio-dhivehi-raajjeyge-adu",
     "safari_tv": "https://j78dp346yq5r-hls-live.5centscdn.com/safari/live.stream/chunks.m3u8",
     "victers_tv": "https://932y4x26ljv8-hls-live.5centscdn.com/victers/tv.stream/victers/tv1/chunks.m3u8",
-    
-    
-    
-   "kairali_we": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/wetv_nim_https/050522/wetv/playlist.m3u8",
-"flowers_tv": "http://103.199.161.254/Content/flowers/Live/Channel(Flowers)/index.m3u8",
-
-"dd_malayalam": "https://d3eyhgoylams0m.cloudfront.net/v1/manifest/93ce20f0f52760bf38be911ff4c91ed02aa2fd92/ed7bd2c7-8d10-4051-b397-2f6b90f99acb/562ee8f9-9950-48a0-ba1d-effa00cf0478/2.m3u8",
-
-"amrita_tv": "https://dr1zhpsuem5f4.cloudfront.net/master.m3u8",
-
-  "24_news": "https://segment.yuppcdn.net/110322/channel24/playlist.m3u8",
-
-"mazhavil_manorama": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/mazhavilmanorama_nim_https/050522/mazhavilmanorama/playlist.m3u8",
-    
- 
-    
-    
-    
-    
+    "kairali_we": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/wetv_nim_https/050522/wetv/playlist.m3u8",
+    "flowers_tv": "http://103.199.161.254/Content/flowers/Live/Channel(Flowers)/index.m3u8",
+    "dd_malayalam": "https://d3eyhgoylams0m.cloudfront.net/v1/manifest/93ce20f0f52760bf38be911ff4c91ed02aa2fd92/ed7bd2c7-8d10-4051-b397-2f6b90f99acb/562ee8f9-9950-48a0-ba1d-effa00cf0478/2.m3u8",
+    "amrita_tv": "https://dr1zhpsuem5f4.cloudfront.net/master.m3u8",
+    "24_news": "https://segment.yuppcdn.net/110322/channel24/playlist.m3u8",
+    "mazhavil_manorama": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/mazhavilmanorama_nim_https/050522/mazhavilmanorama/playlist.m3u8",
     "manorama_news": "http://103.199.161.254/Content/manoramanews/Live/Channel(ManoramaNews)/index.m3u8",
-    
-    
-    
     "aaj_tak": "https://feeds.intoday.in/aajtak/api/aajtakhd/master.m3u8",
-
-"bloomberg_tv": "https://bloomberg-bloomberg-3-br.samsung.wurl.tv/manifest/playlist.m3u8",
-
-"france_24": "https://live.france24.com/hls/live/2037218/F24_EN_HI_HLS/master_500.m3u8",
-
-
-
+    "bloomberg_tv": "https://bloomberg-bloomberg-3-br.samsung.wurl.tv/manifest/playlist.m3u8",
+    "france_24": "https://live.france24.com/hls/live/2037218/F24_EN_HI_HLS/master_500.m3u8",
     "n1_news": "https://best-str.umn.cdn.united.cloud/stream?stream=sp1400&sp=n1info&channel=n1bos&u=n1info&p=n1Sh4redSecre7iNf0&player=m3u8",
     "vom_radio": "https://radio.psm.mv/draair",
 }
-
 
 # 🔄 Streaming function with error handling
 def generate_stream(url):
     process = None
     while True:
         if process:
-            process.kill()  # Stop old FFmpeg instance before restarting
-        
+            process.kill()
+
         process = subprocess.Popen(
             [
                 "ffmpeg", "-reconnect", "1", "-reconnect_streamed", "1",
                 "-reconnect_delay_max", "10", "-fflags", "nobuffer", "-flags", "low_delay",
                 "-i", url, "-vn", "-ac", "1", "-b:a", "40k", "-buffer_size", "1024k", "-f", "mp3", "-"
             ],
-stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=8192
+            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=8192
         )
 
         print(f"🎵 Streaming from: {url} (Mono, 40kbps)")
@@ -127,15 +94,14 @@ stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=8192
             print(f"⚠️ Stream error: {e}")
 
         print("🔄 FFmpeg stopped, restarting stream...")
-        time.sleep(5)  # Wait before restarting
+        time.sleep(5)
 
-# 🌍 API to stream selected station
+# 🌍 API to stream a station
 @app.route("/<station_name>")
 def stream(station_name):
     url = RADIO_STATIONS.get(station_name)
     if not url:
         return "⚠️ Station not found", 404
-    
     return Response(generate_stream(url), mimetype="audio/mpeg")
 
 # 📜 Serve stationlist.xml
@@ -143,6 +109,6 @@ def stream(station_name):
 def serve_stationlist():
     return send_file("stationlist.xml", mimetype="application/xml")
 
-# 🚀 Start Flask server
+# 🚀 Launch the app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
