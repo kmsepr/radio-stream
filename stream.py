9d53bf07ae2a8f96fe6348d4c3283c6ae74d7969
kmsepr <95290980+kmsepr@users.noreply.github.com>
2026-08-20 07:08:05 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 RADIO_STATIONS = {
     "muthnabi_radio": "http://cast4.my-control-panel.com/proxy/muthnabi/stream",
     "radio_nellikka": "https://usa20.fastcast4u.com:2130/stream",
-    "air_kavarati": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio189/chunklist.m3u8",
+    "air_kavarati": "https://d1cvqgmbcpg5yn.cloudfront.net/ffb3825f86c4b9e3/ffb3825f86c4b9e3.m3u8",
     "air_calicut": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio082/chunklist.m3u8",
     "manjeri_fm": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio101/chunklist.m3u8",
     "real_fm": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio083/playlist.m3u8",
